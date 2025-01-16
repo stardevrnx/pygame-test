e117500dfecba7c9fdff43056fedc094902ec1da
Stardev <stardevrnx@gmail.com>
2025-01-16 12:13:41 +0000

    n

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ screen = pygame.display.set_mode((640, 640))
 running = True
 while running:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT
+        if event.type == pygame.QUIT:
             running = False    
